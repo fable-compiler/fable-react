026296bc0d50e14f694fef87d7fc9329ec8cf01f
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2016-09-27 13:17:21 +0200

    Add xmldom bidings to fable-react-native

---
+++ react-native/Fable.Import.XmlDom.fs
@@ -0,0 +1,25 @@
+namespace Fable.Import
+open System
+open Fable.Core
+open Fable.Import.JS
+
+module XmlDom =
+    type DOMParserStatic =
+        [<Emit("new $0()")>] abstract Create: unit -> DOMParser
+        [<Emit("new $0($1...)")>] abstract Create: options: Options list -> DOMParser
+
+    and XMLSerializerStatic =
+        [<Emit("new $0()")>] abstract Create: unit -> XMLSerializer
+
+    and DOMParser =
+        abstract parseFromString: xmlsource: string * ?mimeType: string -> Browser.Document
+
+    and XMLSerializer =
+        abstract serializeToString: node: Browser.Node -> string
+
+    and [<KeyValueList>] Options =
+        | Locator of obj
+        | ErrorHandler of Func<string, obj>
+
+    let [<Import("DOMParser","xmldom")>] DOMParser: DOMParserStatic = failwith "JS only"
+    let [<Import("XMLSerializer","xmldom")>] XMLSerializer: XMLSerializerStatic = failwith "JS only"
