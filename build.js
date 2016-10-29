01dfc2bbaf4ad9738fa4bc615e5b3697ce42ab2f
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2016-10-29 11:11:06 +0200

    Ignore npm directory

--- build.js
+++ build.js
@@ -2,23 +2,13 @@ var path = require("path");
 var fs = require("fs-extra");
 var fable = require("fable-compiler");
 
-function promise(f) {
-    var args = Array.from(arguments).slice(1);
-    return new Promise(function (resolve, reject) {
-        args.push(function (err, data) {
-            if (err) { reject(err); } else { resolve(data); }
-        });
-        f.apply(this, args);
-    });
-}
-
 var targets = {
     all() {
-        return promise(fs.remove, "npm")
+        return fable.promise(fs.remove, "npm")
             .then(_ => fable.compile())
-            .then(_ => promise(fs.copy, "package.json", "npm/package.json"))
-            .then(_ => promise(fs.copy, "README.md", "npm/README.md"))
-            .then(_ => promise(fs.readFile, "RELEASE_NOTES.md"))
+            .then(_ => fable.promise(fs.copy, "package.json", "npm/package.json"))
+            .then(_ => fable.promise(fs.copy, "README.md", "npm/README.md"))
+            .then(_ => fable.promise(fs.readFile, "RELEASE_NOTES.md"))
             .then(line => {
                 var version = /\d[^\s]*/.exec(line)[0];
                 return fable.runCommand("npm", "npm version " + version);
