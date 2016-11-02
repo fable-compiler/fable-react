47d63b818cba1dc1b8548034c404fb8b0619bc1d
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-11-02 14:52:01 +0100

    Add menu

--- react-native-popup-menu/Fable.Helpers.ReactNativePopupMenu.fs
+++ react-native-popup-menu/Fable.Helpers.ReactNativePopupMenu.fs
@@ -30,26 +30,78 @@ module Props =
         | Opened of bool
         interface IMenuProperties
 
+    [<KeyValueList>]
+    type IMenuTriggerProperties =
+        interface end
 
+    [<KeyValueList>]
+    type MenuTriggerProperties =
+        | Text of string
+        interface IMenuTriggerProperties
+        
+    [<KeyValueList>]
+    type IMenuOptionsProperties =
+        interface end
+
+    [<KeyValueList>]
+    type MenuOptionsProperties =
+        inherit IMenuOptionsProperties
+
+    [<KeyValueList>]
+    type IMenuOptionProperties =
+        interface end
+
+    [<KeyValueList>]
+    type MenuOptionProperties =
+        | Text of string
+        interface IMenuOptionProperties
 open Props
 
 /// Creates a MenuContext element
-let inline menuContext (props:IMenuContextProperties list) (onBackdropPress: unit -> unit) children : React.ReactElement<obj> = 
+let inline menuContext (props:IMenuContextProperties list) (onBackdropPress: unit -> unit) (children: React.ReactElement<obj> list): React.ReactElement<obj> = 
     React.createElement(
       BPM.MenuContext,
         JS.Object.assign(
             createObj ["onBackdropPress" ==> onBackdropPress],
             props)
         |> unbox,
-        unbox children) |> unbox
+        unbox(List.toArray children)) |> unbox
 
 /// Creates a Menu element
-let inline menu (props:IMenuProperties list) (onSelect: obj -> unit) children : React.ReactElement<obj> = 
+let inline menu (props:IMenuProperties list) (onSelect: obj -> unit) (children: React.ReactElement<obj> list): React.ReactElement<obj> = 
     React.createElement(
       BPM.Menu,
         JS.Object.assign(
             createObj ["onSelect" ==> onSelect],
             props)
         |> unbox,
-        unbox children) |> unbox
+        unbox(List.toArray children)) |> unbox
+        
+
+/// Creates a MenuTrigger element
+let inline menuTrigger (props:IMenuProperties list) (onPress: unit -> unit) : React.ReactElement<obj> = 
+    React.createElement(
+      BPM.MenuTrigger,
+        JS.Object.assign(
+            createObj ["onPress" ==> onPress],
+            props)
+        |> unbox,
+        unbox [||]) |> unbox
+        
+
+
+/// Creates a MenuOptions element
+let inline menuOptions (props:IMenuOptionsProperties list) (children: React.ReactElement<obj> list): React.ReactElement<obj> = 
+    React.createElement(
+      BPM.MenuOptions,
+        props |> unbox,
+        unbox(List.toArray children)) |> unbox
+
+
+/// Creates a MenuOption element
+let inline menuOption (props:IMenuOptionProperties list) : React.ReactElement<obj> = 
+    React.createElement(
+      BPM.MenuOption,
+        props |> unbox,
+        unbox [||]) |> unbox        
         
--- react-native-popup-menu/Fable.Import.ReactNativePopupMenu.fs
+++ react-native-popup-menu/Fable.Import.ReactNativePopupMenu.fs
@@ -24,9 +24,42 @@ module ReactNativePopupMenu =
         inherit React.ComponentClass<MenuProperties>
 
     and Menu =
-        MenuStatic        
+        MenuStatic
+
+
+    and MenuTriggerProperties =
+        inherit React.Props<MenuTriggerStatic>
+        
+    and MenuTriggerStatic =
+        inherit React.ComponentClass<MenuTriggerProperties>
+
+    and MenuTrigger =
+        MenuTriggerStatic
+
+    and MenuOptionsProperties =
+        inherit React.Props<MenuOptionsStatic>
+        
+    and MenuOptionsStatic =
+        inherit React.ComponentClass<MenuOptionsProperties>
+
+    and MenuOptions =
+        MenuOptionsStatic
+        
+    and MenuOptionProperties =
+        inherit React.Props<MenuOptionStatic>
+        
+    and MenuOptionStatic =
+        inherit React.ComponentClass<MenuOptionProperties>
+
+    and MenuOption =
+        MenuOptionStatic   
 
     type Globals =
         [<Import("MenuContext", "react-native-popup-menu")>] static member MenuContext with get(): MenuContextStatic = failwith "JS only" and set(v: MenuContextStatic): unit = failwith "JS only"
         [<Import("Menu", "react-native-popup-menu")>] static member Menu with get(): MenuStatic = failwith "JS only" and set(v: MenuStatic): unit = failwith "JS only"
+        [<Import("MenuTrigger", "react-native-popup-menu")>] static member MenuTrigger with get(): MenuTriggerStatic = failwith "JS only" and set(v: MenuTriggerStatic): unit = failwith "JS only"
+        [<Import("MenuOptions", "react-native-popup-menu")>] static member MenuOptions with get(): MenuOptionsStatic = failwith "JS only" and set(v: MenuOptionsStatic): unit = failwith "JS only"
+
+        [<Import("MenuOption", "react-native-popup-menu")>] static member MenuOption with get(): MenuOptionStatic = failwith "JS only" and set(v: MenuOptionStatic): unit = failwith "JS only"
+
 
