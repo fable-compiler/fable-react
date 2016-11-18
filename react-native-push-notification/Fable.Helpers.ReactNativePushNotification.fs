6200ef0ee50add8f96eddc245f736a5b810daca9
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-11-18 16:48:49 +0100

    Start with configure

--- react-native-push-notification/Fable.Helpers.ReactNativePushNotification.fs
+++ react-native-push-notification/Fable.Helpers.ReactNativePushNotification.fs
@@ -5,3 +5,19 @@ open Fable.Core
 open Fable.Import
 open Fable.Import.ReactNativePushNotification
 type PN = ReactNativePushNotification.Globals
+
+
+module Props =
+    [<KeyValueList>]
+    type IPushNotificationOptions =
+        interface end
+
+    [<KeyValueList>]
+    type PushNotificationOptions =
+    | OnRegister of obj -> unit
+        interface IPushNotificationOptions
+
+open Props
+
+let inline configure (props: IPushNotificationOptions list) f =
+    PN.PushNotification?configure(props |> unbox)
--- react-native-push-notification/Fable.Import.ReactNativePushNotification.fs
+++ react-native-push-notification/Fable.Import.ReactNativePushNotification.fs
@@ -7,3 +7,13 @@ open Fable.Import.JS
 open Fable.Import.Browser
 
 module ReactNativePushNotification =
+
+    type PushNotification =
+        abstract member configure: PushNotificationOptions -> unit
+
+    and PushNotificationOptions =
+        abstract onRegister: object -> unit   
+
+    type Globals =
+        [<Import("PushNotification", from="react-native-push-notification")>]
+        static member PushNotification with get(): PushNotification = failwith "JS only" and set(v: PushNotification): unit = failwith "JS only"
