6513d5dab4988dd93889cd0cc2e70786ff175515
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-11-18 17:43:21 +0100

    Allow to send push notifications

--- react-native-push-notification/Fable.Helpers.ReactNativePushNotification.fs
+++ react-native-push-notification/Fable.Helpers.ReactNativePushNotification.fs
@@ -18,9 +18,51 @@ module Props =
     [<KeyValueList>]
     type PushNotificationOptions =
     | OnRegister of (obj -> unit)
+    | SenderID of string
+    | PopInitialNotification of bool
+    | RequestPermissions of bool
+    | OnNotification of (obj -> unit)
         interface IPushNotificationOptions
+
+
+    [<KeyValueList>]
+    type ILocalPushNotificationProperties =
+        interface end
+
+    [<KeyValueList>]
+    type LocalPushNotificationProperties =
+    // Android
+    | Id of int
+    | Ticker of string
+    | AutoCancel of bool
+    | LargeIcon of string
+    | SmallIcon of string
+    | BigText of string
+    | SubText of string
+    | Color of string
+    | Vibrate of bool
+    | Vibration of float
+    | Tag of string
+    | Group of string
+    | OnGoing of bool
+    // Android and iOS
+    | Title of string
+    | Message of string
+    | PlaySound of bool
+    | SoundName of string
+    | Number of string
+    | RepeatType of string
+    | Actions of string
+        interface ILocalPushNotificationProperties
+        
 
 open Props
 
-let inline configure (props: IPushNotificationOptions list) f =
-    PN.PushNotification?configure(props |> unbox)
+module PushNotification =
+
+    let inline configure (props: IPushNotificationOptions list) =
+        PN.PushNotification?configure(props |> unbox) |> ignore
+
+
+    let inline localNotification(props: ILocalPushNotificationProperties list) =
+        PN.PushNotification?localNotification(props |> unbox) |> ignore 
