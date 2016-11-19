ba7c3dfa09de9098b345116e3af405165dd59108
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-11-19 17:16:17 +0100

    Add configure method for OneSignal

--- react-native-onesignal/Fable.Helpers.ReactNativeOneSignal.fs
+++ react-native-onesignal/Fable.Helpers.ReactNativeOneSignal.fs
@@ -8,18 +8,29 @@ open Fable.Import.ReactNativeOneSignal
 
 open Fable.Core.JsInterop
 
-type PN = ReactNativeOneSignal.Globals
-
 /// Contains properties for push notifications via OneSignal.
 module Props =
+
+    [<KeyValueList>]
+    type OneSignalID =
+    | UserId of string
+    | PushToke of string
+
     [<KeyValueList>]
     type IOneSignalOptions =
         interface end
-        
+
+    [<KeyValueList>]
+    type OneSignalOptions =
+    | OnIdsAvailable of (OneSignalID -> unit)
+    | OnNotificationOpened of Func<string,obj,bool,unit>
+        interface IOneSignalOptions
 
 open Props
 
 /// Contains functions for push notifications via OneSignal.
 module OneSignal =
 
-    let x = 1
+    /// Configures the push notification system.
+    let inline configure (props: IOneSignalOptions list) =
+        ReactNativeOneSignal.Globals.OneSignal?configure(props |> unbox) |> ignore
