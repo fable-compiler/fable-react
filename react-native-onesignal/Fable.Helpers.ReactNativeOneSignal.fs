fc4514c37acbfc11d278f11b6b7f435073786828
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-11-19 18:04:09 +0100

    enableInAppAlertNotification

--- react-native-onesignal/Fable.Helpers.ReactNativeOneSignal.fs
+++ react-native-onesignal/Fable.Helpers.ReactNativeOneSignal.fs
@@ -30,3 +30,11 @@ module OneSignal =
     /// Configures the push notification system.
     let inline configure (props: IOneSignalOptions list) =
         ReactNativeOneSignal.Globals.OneSignal?configure(props |> unbox) |> ignore
+
+
+    /// Enables In-App Alert Notifications.
+    /// By default this is false and notifications will not be shown when the user is in the app, instead the OneSignalHandleNotificationBlock is fired. 
+    // If set to true notifications will be shown as native alert boxes if a notification is received when the user is in your app. 
+    // The OneSignalHandleNotificationBlock is then fired after the alert box is closed.
+    let inline enableInAppAlertNotification (enabled:bool) =
+        ReactNativeOneSignal.Globals.OneSignal?enableInAppAlertNotification(enabled) |> ignore
