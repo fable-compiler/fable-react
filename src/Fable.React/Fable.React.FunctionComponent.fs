1066f3d3fd92ec8936f557ef1a31bdd877b683fa
Nick Dunets <nick@chaldal.net>
2024-05-24 10:42:36 +1000

    move impl of Of into the cache class to reduce bundle size

--- src/Fable.React/Fable.React.FunctionComponent.fs
+++ src/Fable.React/Fable.React.FunctionComponent.fs
@@ -15,9 +15,40 @@ type FunctionComponentPreparedRenderFunctionCache() =
 #endif
         cache
 
-    static member GetOrAdd(key: string, valueFactory: string->'T): 'T =
-        if cache.has(key) then cache.get(key) :?> 'T
-        else let v = valueFactory key in cache.set(key, box v) |> ignore; v
+    static member GetOrAdd(
+            cacheKey: string,
+            displayName: string,
+            render: 'Props -> ReactElement,
+            memoizeWith: ('Props -> 'Props -> bool) option,
+            withKey: ('Props -> string) option,
+            [<CallerMemberName>] ?__callingMemberName: string) =
+        let prepareRenderFunction () =
+            render?displayName <- displayName
+            let elemType =
+                match memoizeWith with
+                | Some areEqual ->
+#if DEBUG
+                    // In development mode, force rerenders always when HMR is fired
+                    let areEqual x y =
+                        not FunctionComponentPreparedRenderFunctionCache.IsHMRApplied && areEqual x y
+#endif
+                    let memoElement = ReactElementType.memoWith areEqual render
+                    memoElement?displayName <- "Memo(" + displayName + ")"
+                    memoElement
+                | None -> ReactElementType.ofFunction render
+            fun props ->
+                let props =
+                    match withKey with
+                    | Some f -> props?key <- f props; props
+                    | None -> props
+                ReactElementType.create elemType props []
+
+        if cache.has(cacheKey) then
+            cache.get(cacheKey) :?> ('Props -> ReactElement)
+        else
+            let v = prepareRenderFunction ()
+            cache.set(cacheKey, box v) |> ignore
+            v
 
     [<Emit("""typeof module === 'object'
 && typeof module.hot === 'object'
@@ -68,28 +99,6 @@ type FunctionComponent =
 #endif
                     ): 'Props -> ReactElement =
 #if FABLE_COMPILER
-        let prepareRenderFunction (_: string) =
-            let displayName = defaultArg displayName __callingMemberName.Value
-            render?displayName <- displayName
-            let elemType =
-                match memoizeWith with
-                | Some areEqual ->
-#if DEBUG
-                    // In development mode, force rerenders always when HMR is fired
-                    let areEqual x y =
-                        not FunctionComponentPreparedRenderFunctionCache.IsHMRApplied && areEqual x y
-#endif
-                    let memoElement = ReactElementType.memoWith areEqual render
-                    memoElement?displayName <- "Memo(" + displayName + ")"
-                    memoElement
-                | None -> ReactElementType.ofFunction render
-            fun props ->
-                let props =
-                    match withKey with
-                    | Some f -> props?key <- f props; props
-                    | None -> props
-                ReactElementType.create elemType props []
-
         // Cache the render function to prevent recreating the component every time when FunctionComponent.Of
         // is called inside another function (including generic values: let MyCom<'T> = ...)
         let cacheKey =
@@ -97,8 +106,9 @@ type FunctionComponent =
             "#L" + (string __callingSourceLine.Value) +
             // direct caller can also be generic, need separate cached func per 'Props argument
             ";" + typeof<'Props>.FullName
+        let displayName = defaultArg displayName __callingMemberName.Value
 
-        FunctionComponentPreparedRenderFunctionCache.GetOrAdd(cacheKey, prepareRenderFunction)
+        FunctionComponentPreparedRenderFunctionCache.GetOrAdd (cacheKey, displayName, render, memoizeWith, withKey)
 #else
         let elemType = ReactElementType.ofFunction render
         fun props ->
